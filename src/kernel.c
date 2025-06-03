bb21879d658d2708d321e59bf027618c5aa94119
Jofanka Al-Kautsar P A <jofankaapa@gmail.com>
2025-06-03 23:51:32 +0700

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -1,22 +1,47 @@
+/// === kernel.c ===
 #include "shell.h"
 #include "kernel.h"
 
+
+
+void printString(char *str) {
+  int i = 0;
+  while (str[i] != 0) {
+    interrupt(0x10, 0x0E00 + str[i], 0, 0, 0);
+    i++;
+  }
+}
+
+void readString(char *buf) {
+  int i = 0;
+  char c;
+  while (1) {
+    c = interrupt(0x16, 0, 0, 0, 0);
+    if (c == 0x0D) {
+      buf[i] = 0;
+      printString("\r\n");
+      return;
+    } else if (c == 0x08 && i > 0) {
+      i--;
+      printString("\b \b");
+    } else if (c >= 32 && c <= 126) {
+      buf[i++] = c;
+      interrupt(0x10, 0x0E00 + c, 0, 0, 0);
+    }
+  }
+}
+
+void clearScreen() {
+  int i;
+  for (i = 0; i < 25 * 80; i++) {
+    putInMemory(0xB800, i * 2, ' ');
+    putInMemory(0xB800, i * 2 + 1, 0x07);
+  }
+  interrupt(0x10, 0x0200, 0, 0, 0); // reset cursor
+}
+
 int main() {
   clearScreen();
-  shell();
-}
-
-void printString(char *str)
-{
-  //TODO: Implementasi fungsi untuk mencetak string
-}
-
-void readString(char *buf)
-{
-  //TODO: Implementasi fungsi untuk membaca string
-}
-
-void clearScreen()
-{
-  //TODO: Implementasi fungsi untuk membersihkan layar
+  // shell();
+  return 0;
 }
