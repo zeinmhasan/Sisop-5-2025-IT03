ba9d5ba557b9a1333c4615f585dae6d0fd83ca93
Zein Muhammad Hasan <hasro71@gmail.com>
2025-06-09 22:31:11 +0700

    Update kernel.asm

--- src/kernel.asm
+++ src/kernel.asm
@@ -1,16 +1,10 @@
-; kernel.asm
+; kernel.asm - VERSI FINAL STABIL
 
 global _putInMemory
-global _interrupt
 global _getBiosTick
-
-; unsigned int getBiosTick()
-_getBiosTick:
-        mov ah, 0x00
-        int 0x1A
-        mov ax, dx
-        mov dx, cx
-        ret
+global _bios_print_char
+global _bios_read_char
+global _bios_set_cursor
 
 ; void putInMemory(int segment, int address, char character)
 _putInMemory:
@@ -26,24 +20,41 @@ _putInMemory:
 	pop bp
 	ret
 
-; int interrupt(int number, int AX, int BX, int CX, int DX)
-_interrupt:
-	push bp
-	mov bp,sp
-	mov ax,[bp+4]
-	push ds
-	mov bx,cs
-	mov ds,bx
-	mov si,intr
-	mov [si+1],al
-	pop ds
-	mov ax,[bp+6]
-	mov bx,[bp+8]
-	mov cx,[bp+10]
-	mov dx,[bp+12]
+; unsigned int getBiosTick()
+_getBiosTick:
+    mov ah, 0x00
+    int 0x1A
+    mov ax, dx
+    ret
 
-intr:	int 0x00
+; FUNGSI BARU: Mencetak satu karakter menggunakan BIOS teletype
+; void bios_print_char(char c)
+_bios_print_char:
+    push bp
+    mov bp, sp
+    mov al, [bp+4]  ; Ambil karakter dari argumen
+    mov ah, 0x0E      ; Fungsi teletype
+    mov bh, 0         ; Halaman 0
+    int 0x10
+    pop bp
+    ret
 
-	mov ah,0
-	pop bp
-	ret
+; FUNGSI BARU: Membaca satu karakter dari keyboard
+; int bios_read_char() -> mengembalikan AX (Scan Code & ASCII)
+_bios_read_char:
+    mov ah, 0x00      ; Tunggu ketikan keyboard
+    int 0x16
+    ret
+
+; FUNGSI BARU: Mengatur posisi kursor
+; void bios_set_cursor(int row, int col)
+_bios_set_cursor:
+    push bp
+    mov bp, sp
+    mov dh, [bp+4]  ; Ambil baris (row)
+    mov dl, [bp+6]  ; Ambil kolom (col)
+    mov bh, 0         ; Halaman 0
+    mov ah, 0x02      ; Fungsi set kursor
+    int 0x10
+    pop bp
+    ret
